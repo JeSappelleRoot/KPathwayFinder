d025423398632ec60bb7c7bce90e069787451cee
JeSappelleRoot <joseph.morin.it@gmail.com>
2019-10-30 09:51:30 -0500

    Add verbosity parameter for KEGG searcher, add condition if KEGG return a non valid research

--- KPathwayFinder.py
+++ KPathwayFinder.py
@@ -3,59 +3,67 @@ from bioservices.kegg import KEGG
 
 
 
-def enzymeInfo(code):
+def enzymeInfo(code, ignored,verbosity):
 # Function to get info about an enzyme, from the code
+# This function return a double list
 
     # Intialize KEGG searcher
-    kSearch = KEGG()
+    kSearch = KEGG(verbose=verbosity)
 
-    # List with ignored pathways, can be empty
-    ignoredPathway = ['ko01100']
+
 
     # Get result and parse it in a dictionnary
     print(f"[+] Get info about enzyme {code}")
+
+
+    
     result = kSearch.get(code)
-    dictResult = kSearch.parse(result)
-    
-    # Get all pathways as keys in dictionnary
-    pathwayList = list(dictResult['PATHWAY'].keys())
-    
 
-    # Create prefix list, info about enzyme herself
-    prefixList = []
-
-    # Add code at the begining of the list
-    prefixList.append(code)
-
-    # If name is present as key, else 'NONAME' insted
-    if 'NAME' in dictResult.keys():
-        prefixList.append(dictResult['NAME'])
+    if type(result) is int:
+        return False
     else:
-        prefixList.append('NONAME')
 
-    # If definition is present as key, else 'NODEFINITION' insted
-    if 'DEFINITION' in dictResult.keys():
-        prefixList.append(dictResult['DEFINITION'])
-    else:
-        prefixList.append('NODEFINITION')
+        dictResult = kSearch.parse(result)
+        
+        # Get all pathways as keys in dictionnary
+        pathwayList = list(dictResult['PATHWAY'].keys())
+        
 
+        # Create prefix list, info about enzyme herself
+        prefixList = []
 
-    # Create final list, which contain : 
-    # - prefix (info about enzyme) 
-    # - suffix list (info about each enzyme's pathways)
-    finalList = []
+        # Add code at the begining of the list
+        prefixList.append(code)
 
-    for pathway in pathwayList:
-        if pathway not in ignoredPathway:
-            print(f"  [-] Get info about {pathway} pathway")
-            suffixList = pathwayInfo(pathway)
+        # If name is present as key, else 'NONAME' insted
+        if 'NAME' in dictResult.keys():
+            prefixList.append(dictResult['NAME'])
         else:
-            print(f"  [!] Ignored pathway : {pathway}")
+            prefixList.append('NONAME')
 
-        finalList.append(prefixList + suffixList)
+        # If definition is present as key, else 'NODEFINITION' insted
+        if 'DEFINITION' in dictResult.keys():
+            prefixList.append(dictResult['DEFINITION'])
+        else:
+            prefixList.append('NODEFINITION')
 
-    
-    return finalList
+
+        # Create final list, which contain : 
+        # - prefix (info about enzyme) 
+        # - suffix list (info about each enzyme's pathways)
+        finalList = []
+
+        for pathway in pathwayList:
+            if pathway not in ignored:
+                print(f"  [-] Get info about {pathway} pathway")
+                suffixList = pathwayInfo(pathway)
+            else:
+                print(f"  [!] Ignored pathway : {pathway}")
+
+            finalList.append(prefixList + suffixList)
+
+        
+        return finalList
 
 
 # -------------------------------------------------------------------------------------------------
@@ -102,18 +110,28 @@ def pathwayInfo(code):
 
 
 
-
-
-
 source = r'/home/scratch/Downloads/source.txt'
+sourceList = ['K10','K00001', 'K0','K00002', 'K00003']
+# List with ignored pathways, can be empty
+ignoredPathway = ['ko01100']
 
-
+# Seet verbosity of KEGG searcher
+v = False
+# Initialize the main list, wich contains other list about enzyme + pathways
 enzymeList = []
 
-sourceList = ['K00001', 'K00002', 'K00003']
+
 for enzyme in sourceList:
-    for liste in enzymeInfo(enzyme):
-        enzymeList.append(liste)
+    # Get info about the enzyme
+    aboutEnzyme = enzymeInfo(enzyme, ignoredPathway, v)
+    # If the function return False, the KEGG request is not valid and pass at the other enzyme
+    if aboutEnzyme == False:
+        print(f"[!] Something wrong happened with enzyme {enzyme} (skipped)\n")
+        pass
+    else:
+        # Else, for each list return by the function, add these in the main list
+        for liste in aboutEnzyme:
+            enzymeList.append(liste)
 
 
 
